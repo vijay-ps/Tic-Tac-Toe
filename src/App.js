f5006af7c6a8e54706fcc2cd6df5f1c657da7d0a
vijay-ps <vijay.ps2022@vitstudent.ac.in>
2023-08-15 18:55:16 +0530

    commiting files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from "react";
+import "./App.css";
+import Board from "./Board";
+import Info from "./Info";
 function App() {
+  const [reset, setReset] = useState(false);
+  const [winner, setWinner] = useState("");
+
+  const resetBoard = () => {
+    setReset(true);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className={`winner ${winner !== "" ? "" : "shrink"}`}>
+        <div className="winner-text">{winner}</div>
+        <button onClick={() => resetBoard()}>Reset Board</button>
+      </div>
+      <Board
+        reset={reset}
+        setReset={setReset}
+        winner={winner}
+        setWinner={setWinner}
+      ></Board>
+      <Info></Info>
     </div>
   );
 }
